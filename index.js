743a278365599e2aeec48c8a58cbedb273bd6684
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-02-01 12:46:56 -0800

    Add functionality to the Reset button
    It now resets the sketch pad to blank when clicked.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let container = document.querySelector('#container');
 document.querySelector('#change-resolution').onclick = changeResolution;
 
 function changeResolution() {
-    let resolution = Number(prompt("Please enter a resolution for your sketch pad. Maximum: 100", "16"));
+    let resolution = Number(prompt("Please enter the number of squares per side for your sketch pad. Maximum: 100", "16"));
     resolution = (resolution <= 100) ? resolution : 100;
 
     // Clear existing content in the container
@@ -16,14 +16,30 @@ function changeResolution() {
         for (let j = 0; j < resolution; j++) {
             let div = document.createElement('div');
             div.classList.add('square-div');
-    
-            if (j % resolution === 0) div.style.backgroundColor = 'lime';
-            // div.addEventListener('mouseover', () => {
-            //     div.style.backgroundColor = 'black';
-            // });
+            setBackgroundColor(div, 'lightblue');
+            
+            // To easily keep track if the square grid's been rendered properly
+            // if (j % resolution === 0) div.style.backgroundColor = 'lime';
+            
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'black';
+            });
 
             rowContainer.appendChild(div);
         }
         container.appendChild(rowContainer);
     }
+}
+
+document.querySelector('#reset').onclick = resetSketchPad;
+
+function resetSketchPad() {
+    let squareDivs = document.querySelectorAll('.square-div');
+    for (let sqDiv of squareDivs) {
+        setBackgroundColor(sqDiv, 'lightblue');
+    }
+}
+
+function setBackgroundColor(element, color) {
+    element.style.backgroundColor = color;
 }
